1b1403d835fce0846c4190e607921656ad6a2ae8
kjishereeee <67011137@kmitl.ac.th>
2025-06-25 01:00:41 +0700

    upload

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,11 +1,16 @@
-i = 0
-while i < 11 :
-    print(f"Table de {i}: ", end="")
-    j = 0
-    while j < 11:
-        if j == 10:
-            print(f"{i * j}")
-        else:   
-            print(f"{i * j}", end=" ")
-        j += 1
-    i += 1
+import sys
+
+if len(sys.argv) > 1:
+    print("none")
+else:
+    i = 0
+    while i < 11 :
+        print(f"Table de {i}: ", end="")
+        j = 0
+        while j < 11:
+            if j == 10:
+                print(f"{i * j}")
+            else:   
+                print(f"{i * j}", end=" ")
+            j += 1
+        i += 1
