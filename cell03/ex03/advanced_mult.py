195e0e12612806c79863977bc4de59cc090724d6
kjishereeee <67011137@kmitl.ac.th>
2025-06-25 00:48:42 +0700

    update

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,7 +1,11 @@
-for i in range(0, 11):
+i = 0
+while i < 11 :
     print(f"Table de {i}: ", end="")
-    for j in range(0, 11):
+    j = 0
+    while j < 11:
         if j == 10:
             print(f"{i * j}")
         else:   
             print(f"{i * j}", end=" ")
+        j += 1
+    i += 1
