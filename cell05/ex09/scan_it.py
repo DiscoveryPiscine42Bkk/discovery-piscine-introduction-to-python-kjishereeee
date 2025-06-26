08fe4c12ddac05d9fb9ad98c8383d03973c0debe
kjishereeee <67011137@kmitl.ac.th>
2025-06-26 21:02:53 +0700

    update

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 import sys
+import sys
 
-if len(sys.argv) < 3 :
+if len(sys.argv) == 3:
+    find = sys.argv[1] 
+    word = sys.argv[2] 
+    count = word.count(find)
+    if count == 0:
+        print("none")
+    else:
+        print(count)
+
+else:
     print("none")
-else :
-    print(len(sys.argv)-1)
---
+++ cell05/ex14/free_range.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+import sys
+if len(sys.argv) == 3 :
+    for i in range(sys.arvg[1])
