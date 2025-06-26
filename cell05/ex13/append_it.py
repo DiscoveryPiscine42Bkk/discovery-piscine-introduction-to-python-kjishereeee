d5814a0e76ab175429643044d852ba328c9d6862
kjishereeee <67011137@kmitl.ac.th>
2025-06-26 20:48:29 +0700

    update

---
+++ cell05/ex13/append_it.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+if len(sys.argv) < 2:
+    print("none")
+else:
+    for i in sys.argv[1:] :
+        if i.endswith("ism") :
+            print(end='')
+        else :
+            print(i+"ism")
