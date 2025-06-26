4b185c2f9720769e82a2cdfdef733236f00dc214
kjishereeee <67011137@kmitl.ac.th>
2025-06-26 19:23:37 +0700

    update

--- cell05/ex10/parameter_matching.py
+++ cell05/ex10/parameter_matching.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
 
 import sys
-
-x = sys.argv[1]
-
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
     print("none")
 else:
-    y = input("What was the parameter? ")
-    if x == y:
+    x = input("What was the parameter? ")
+    if x == sys.argv[1]:
         print("Good job!")
     else:
         print("Nope, sorry...")
