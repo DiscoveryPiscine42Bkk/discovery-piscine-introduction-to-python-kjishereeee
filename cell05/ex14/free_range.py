b6d662f1214f2c935cc42f89e8d604d539b7633f
kjishereeee <67011137@kmitl.ac.th>
2025-06-26 21:12:15 +0700

    update

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 import sys
+list1 = []
 if len(sys.argv) == 3 :
-    for i in range(sys.arvg[1])
+    for i in range(int(sys.argv[1]), (int(sys.argv[2])+1)):
+        list1.append(i)
+    print(list1)
+else:
+    print("none")
