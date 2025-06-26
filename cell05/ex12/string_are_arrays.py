fcdca602ad12b86ac65817970cb413e652a014f0
kjishereeee <67011137@kmitl.ac.th>
2025-06-26 20:29:38 +0700

    update

--- cell05/ex12/string_are_arrays.py
+++ cell05/ex12/string_are_arrays.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import sys
-
-if len(sys.argv) < 1:
+if len(sys.argv) < 2 or "z" not in sys.argv[1]:
     print("none")
 else:
     for i in sys.argv[1]:
         if i.count("z") == 1:
             print("z",end='')
+    print()
